b115b309fbae291eb96ae1ce9f604b5deed5ff0c
Old TyT <42464565+OldTyT@users.noreply.github.com>
2023-11-07 13:35:23 +0300

    chore: fixing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,12 @@ echo "$SSH_KEY_PRIVATE" > ~/.ssh/id_rsa && \
 chmod 600 /root/.ssh/id_rsa && \
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config && \
 echo -e "Host gitlab.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config && \
-mkdir pages && \
-hugo  --destination=pages --baseURL=$BASEURL && \
+mkdir /pages && \
+hugo  --destination=/pages --baseURL=$BASEURL && \
 git switch pages && \
-mv .git pages && \
-cd pages && \
+mv .git /pages && \
+cd /pages && \
+git add . && \
 git config --global user.email "nezlobin118@gmail.com" && \
 git config --global user.name "OldTyT" && \
 git commit -m "chore: auto generate" && \
